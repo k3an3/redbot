3e879d78048af6fdace33c31c98b61780e045759
Keane O'Kelley <kokelley@iastate.edu>
2017-12-21 17:51:25 -0600

    Works better

--- redbot/modules/nmap.py
+++ redbot/modules/nmap.py
@@ -1,21 +1,32 @@
 from time import sleep
 from typing import Dict
 
+import datetime
+from libnmap.parser import NmapParser, NmapParserException
 from libnmap.process import NmapProcess
 
 from redbot.async import celery
 from redbot.models import targets
 from redbot.web.web import socketio, send_msg
 
+last_scan = None
+
 
 @celery.task(bind=True)
-def nmap_scan(self, target: Dict[str, str], options: str = 'sT -n') -> None:
+def nmap_scan(self, target: Dict[str, str], options: str = 'sT -n -T5') -> None:
     nm = NmapProcess(target['range'], options=options)
     nm.run_background()
     while nm.is_running():
         self.update_state(state="PROGRESS", meta={'progress': nm.progress,
-                          'target': target['name']})
+                                                  'target': target['name']})
         sleep(2)
+    try:
+        report = NmapParser.parse(nm.stdout)
+    except NmapParserException as e:
+        print(e)
+    hosts = [(host.address, host.get_open_ports()) for host in report.hosts if host.is_up()]
+    self.update_state(state="RESULTS", meta={'hosts': hosts,
+                                             'target': target['name']})
 
 
 def push_update(body):
@@ -29,3 +40,5 @@ def run_scans() -> None:
     for status in r:
         status.get(on_message=push_update, propagate=False)
     send_msg('Scan finished.')
+    global last_scan
+    last_scan = datetime.datetime.now()
--- redbot/web/static/js/main.js
+++ redbot/web/static/js/main.js
@@ -22,6 +22,47 @@ ws.on('nmap progress', function(data) {
         loading.show();
         loadingbar.attr('style', 'width: ' + data.result.progress + '%');
         $('#target').html("Scanning \"" + data.result.target + "\"");
+    } else if (data.status == 'RESULTS') {
+        data.result.hosts.forEach(function(a) {
+            cy.add({
+                group: "nodes",
+                data: {
+                    id: a[0],
+                    parent: data.result.target,
+                },
+                style: {
+                    'background-color': 'blue',
+                    'label': a[0],
+                }
+            });
+            a[1].forEach(function(b) {
+                cy.add({
+                    group: "nodes",
+                    data: {
+                        id: a[0] + b,
+                        parent: a[0]
+                    },
+                    style: {
+                        'width': 20,
+                        'height': 20,
+                        'background-color': 'white',
+                        'color': 'white',
+                        'label': b[0],
+                    }
+                });
+            });
+            cy.add({
+                    group: "edges",
+                    data: {
+                        source: "redbot",
+                        target: a[0],
+                    }
+                }
+            );
+
+            cy.layout(options).run();
+            cy.fit();
+        });
     } else if (data.status == 'SUCCESS') {
         loading.hide();
         $('#target').html('');
@@ -33,11 +74,30 @@ $('#forcescan').click(function() {
     $(this).fadeOut();
     ws.emit('run nmap');
     loading.show();
+    cy.destroy();
+    cy = cytoscape({
+        container: $('#cy'),
+    });
+    cy.add({
+        group: "nodes",
+        position: {
+            x: 0,
+            y: 0,
+        },
+        data: {
+            id: "redbot",
+        },
+        style: {
+            'background-color': 'red',
+            'shape': 'vee',
+            'label': 'redbot',
+        }
+    });
 });
 
 ws.on('disconnect', function(data) {
     display_message({class: 'danger',
-                     content: 'WebSocket disconnected.'});
+        content: 'WebSocket disconnected.'});
 });
 
 ws.on('connect', function(data) {
