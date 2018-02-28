c9b790ac73526e9694dab95fa02616b9dfad8c1d
Keane O'Kelley <kokelley@iastate.edu>
2018-02-28 16:17:17 -0600

    Update install

---
+++ scripts/install_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install lib{ffi,xml{sec1,2}}-dev python3-dev python3-pip nmap
--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-from pip.req import parse_requirements
-from setuptools import setup
+import os
+from shutil import copyfile
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        if os.path.isdir('/etc/systemd/system'):
+            path = os.path.dirname(os.path.realpath(__file__))
+            for file in os.listdir(os.path.join(path, 'scripts')):
+                copyfile(os.path.join('scripts', file), os.path.join('/etc/systemd/system', file))
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='redbot',
     version='0.0.1',
-    packages=['redbot'],
-    install_requires=parse_requirements('requirements.txt'),
+    packages=find_packages(),
+    install_requires=required,
     url='',
-    scripts=['run.py'],
     license='MIT',
     author="Keane O'Kelley",
     author_email='kokelley@iastate.edu',
-    description='Automated attack and traffic generation'
+    description='Automated attack and traffic generation',
+    scripts=['redbot-web', 'redbot-celery', 'redbot-celerybeat'],
+    cmdclass={'install': CustomInstallCommand}
 )
