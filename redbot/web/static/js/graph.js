8f6a89995ab56b491678b8d1f3f6103961e7e9d3
Keane O'Kelley <keane.m.okelley@gmail.com>
2018-03-21 22:48:35 -0500

    Sort things

--- redbot/web/static/js/graph.js
+++ redbot/web/static/js/graph.js
@@ -41,6 +41,41 @@ $('#fit').click(function() {
     cy.fit();
 });
 
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("hosts");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[0];
+            y = rows[i + 1].getElementsByTagName("TD")[0];
+            // Check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // I so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
 ws.on('hosts', function(data) {
     update_scantime(data.scantime);
     $.each(data.notes, function(host, notes) {
@@ -52,6 +87,7 @@ ws.on('hosts', function(data) {
     init_graph();
     var table = $('#hosts tbody');
     table.empty();
+    console.log(data.data);
     $.each(data.data, function(host, data) {
         graph_host([host, data.ports], data.target);
         if (data.hostname != null)
@@ -59,6 +95,7 @@ ws.on('hosts', function(data) {
         table.append('<tr><td>' + data.target + '</td><td>' + host + '</td><td>' + parse_ports(data.ports) + '</td><td id="notes-' + host + '"></td></tr>');
     });
     $('[data-toggle="tooltip"]').tooltip();
+    sortTable();
 });
 
 function graph_host(host, target) {
