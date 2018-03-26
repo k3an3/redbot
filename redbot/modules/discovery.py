b132230b718eea9bfd35f03689cdb2f92400e1f5
Keane OKelley <keane.m.okelley@gmail.com>
2018-03-25 21:46:46 -0500

    Update 'redbot/modules/discovery.py'

--- redbot/modules/discovery.py
+++ redbot/modules/discovery.py
@@ -8,7 +8,7 @@ Hosts:
         hostname: {
                         'ports': {
                             portnum: {
-                                banner: "",
+                                'banner': '',
                             }
                         }
                         'target': target,
